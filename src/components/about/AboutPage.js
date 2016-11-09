1c446b0486d18e95bcf7decd91bed16987045ad9
Zephyris13 <gaffer.tape@gmail.com>
2016-11-09 22:34:20 +0100

    Initial commit

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class AboutPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This application user React, Redux, React Router and a variety of other helpful libraries.</p>
+      </div>
+    );
+  }
+}
