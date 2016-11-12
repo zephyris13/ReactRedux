3b34a8d60c0c946365d142b2fd95699326083dc1
Shaun Tabone <stabone@crimsonwing.com>
2016-11-12 21:33:20 +0100

    Added missing packages

---
+++ readme.md
@@ -0,0 +1,15 @@
+# React, Redux, Immutable, ES6, Sass Boilerplate
+
+## Install
+```sh
+$ npm install
+```
+
+## Run
+```sh
+$ npm start
+```
+
+## License
+
+MIT © Zephyris13
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -5,7 +5,7 @@ export default class HomePage extends Component {
   render() {
     return (
       <div className="jumbotron">
-        <h1>Administration</h1>
+        <h1>Welcome</h1>
         <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
         <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
       </div>
